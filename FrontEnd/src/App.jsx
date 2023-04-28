ff6a7bb88466ba0e5a5e47666ec7a979031a13cc
jrayo-2020291 <jrayo-2020291@knal.edu.gt>
2023-04-28 17:40:02 -0600

    Se agregaron otras dependencias al App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import './App.js'
+import 'https://kit.fontawesome.com/32c2859f80.js'
+import './assets/favicon.png'
+
 
 function App() {
   return (
